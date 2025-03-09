0e1a4846498050a4c844ef398b2313b9e7882b79
mariane <marianemireli2006@gmail.com>
2025-03-09 03:41:08 -0300

    feat: atualizando ProdutoDao e ProdutoView

--- src/view/CadastroProdutoView.java
+++ src/view/CadastroProdutoView.java
@@ -1,7 +1,11 @@
 package view;
 
+import dao.ProdutoDAO;
+import model.Produto;
+
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.IOException;
 
 public class CadastroProdutoView extends JDialog {
     private JPanel contentPane;
@@ -11,17 +15,33 @@ public class CadastroProdutoView extends JDialog {
     private JTextField campoNome;
     private JTextField campoTipo;
     private JButton btnVizualizarProdutos;
+    private ProdutoDAO produtoDAO;
 
     public CadastroProdutoView() {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonSalvar);
+        produtoDAO = new ProdutoDAO();
 
 
         buttonSalvar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if(validarCampos()){
                     System.out.println("Produto salvo: " + campoNome.getText());
+                    String codString = campoCodigo.getText();
+                    int cod = Integer.parseInt(codString);
+                    String nome = campoNome.getText();
+                    String tipo = campoTipo.getText();
+                    Produto produto = new Produto(cod, nome , tipo);
+                    try {
+                        produtoDAO.adicionarProduto(produto);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (ClassNotFoundException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+
                     onOK();
                 }
 
